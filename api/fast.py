67fec4147e1c99583dd3b93eeb5e3d4e83486c68
Joao Canedo <jpfcanedo@gmail.com>
2023-09-13 15:09:42 -0300

    API is working like 90% accuracy

--- api/fast.py
+++ api/fast.py
@@ -1,11 +1,13 @@
 import pandas as pd
 import numpy as np
 from fastapi import FastAPI, File, UploadFile
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from fastapi.middleware.cors import CORSMiddleware
 from tensorflow.keras.models import load_model
 #import python-multipart
 import cv2
 import io
+import os
 
 app = FastAPI()
 
@@ -19,6 +21,13 @@ app.add_middleware(
     allow_headers=["*"],  # Allows all headers
 )
 
+def translate_output(y_pred):
+
+    labels = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'I', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y']
+    max_index = np.argmax(y_pred,axis=1)
+    translated_output = [labels[idx] for idx in max_index]
+    return translated_output
+
 @app.post("/predict")
 
 async def predict(
@@ -31,32 +40,27 @@ async def predict(
 
     cv2_img = cv2.imdecode(nparr, cv2.IMREAD_COLOR) # type(cv2_img) => numpy.ndarray
 
-    cv2_img = cv2_img/255
-
-    desired_shape = (64, 64, 3)
-
-    resized_img = cv2.resize(cv2_img, dsize = (desired_shape[1], desired_shape[0]))
-
-    resized_img = np.expand_dims(resized_img,axis=0)
+    cv2.imwrite('api/img_test/test/img.jpg', cv2_img)
 
     """Function that makes the prediction of LibrAI and feeds the API"""
 
-    # Load the model
+    ## Load the model
 
     model = load_model("api/model_v1")
 
-    y_pred = model.predict(resized_img)
+    im_shape = (64, 64)
+    seed = 10
 
-    labels = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'I', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y']
+    IMG_DIR = 'api/img_test'
 
-    max_index = np.argmax(y_pred,axis=1)
+    img_test = ImageDataGenerator(rescale=1/255)
+    img_test = img_test.flow_from_directory(IMG_DIR, target_size=im_shape, shuffle=False, seed=seed)
 
-    translated_result = labels[max_index[0]]
+    test = model.predict(img_test)
 
-    #print("-------------------------------------------")
-    #print(y_pred)
-    #print(max_index)
-    #print(resized_img)
+    translated_result = translate_output(test)
+
+    os.remove('api/img_test/test/img.jpg')
 
     return {"prediction": translated_result}
 
